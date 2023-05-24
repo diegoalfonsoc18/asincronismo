818432d6d6b5940626898bee840c407312716d8d
Diego Alfonso <dlalfonso57@hotmail.com>
2023-05-23 21:01:36 -0500

    api platzi

---
+++ src/promise/index.js
@@ -0,0 +1,24 @@
+const promise = new Promise(function (resolve, reject) {
+  resolve("Hey!");
+});
+
+const cows = 7;
+
+const countCows = new Promise(function (resolve, reject) {
+  if (cows > 10) {
+    resolve(`We have ${cows} cows on the farm`);
+  } else {
+    reject("There is no cows on the farm");
+  }
+});
+
+countCows
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("Finally");
+  });
